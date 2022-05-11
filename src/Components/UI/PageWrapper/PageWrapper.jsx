dbee0481b3312286cb154165cb483bf69e91153e
saadwaqas688 <saadwaqas688@gmail.com>
2022-05-12 04:24:50 +0500

    improved architecture

---
+++ src/Components/UI/PageWrapper/PageWrapper.jsx
@@ -0,0 +1,150 @@
+// import * as React from 'react';
+// import Box from '@mui/material/Box';
+// import { createTheme, Stack } from '@mui/material';
+// import Sidebar from './Sidebar';
+// import Navbar from './Navbar';
+// import { ThemeProvider } from '@emotion/react';
+
+
+
+// export default function PageWrapper(props) {
+//   const [mode, setMode] = React.useState("light");
+//   const darkTheme = createTheme({
+//     palette: {
+//       mode: mode,
+//     },
+//   });
+//   return (
+//     <ThemeProvider theme={darkTheme}>
+      
+
+//           <Box bgcolor={"background.default"} color={"text.primary"}>
+//         <Navbar />
+//         <Stack direction="row" spacing={2} justifyContent="space-between">
+//         <Sidebar setMode={setMode} mode={mode}/>
+//         {props.children}
+//         </Stack>
+//       </Box>
+//       </ThemeProvider>
+
+//   );
+// }
+
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import Sidebar from '../SideBar/Sidebar';
+
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+export default function PageWrapper(props){
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(true);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <AppBar position="fixed" open={open} sx={{bgcolor:"#ff6699"}}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6"  component="div" sx={{fontWeight: '1000'}}>
+          LUMINA ADMIN
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Sidebar />
+
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+          {props.children}
+      </Main>
+    </Box>
+  );
+}
---
+++ src/Components/Views/AdminUsers/viewAllAdminUsers.jsx
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from 'react'
+import { Skeleton, Stack } from '@mui/material';
+import PageWrapper from '../../UI/PageWrapper/PageWrapper';
+import AddAdminUser from './addAdminUser';
+import { getService, updateService } from '../../../services/services';
+import Popup from '../../UI/Popup/Popup';
+import Table from '../../UI/Table/Table';
+
+
+const headCells = [
+    { id: 'fullName', label: 'Email' },
+    { id: 'price', label: 'Created At' },
+    { id: 'Actions', label: 'Actions', disableSorting: true },
+
+]
+
+export default function ViewAllAdminUsers() {
+    const [records, setRecords] = useState()
+    const [loading, setLoading] = useState(false)
+    const [openPopup, setOpenPopup] = useState(false)
+
+
+
+
+    const handelFetch = async() => {
+        setLoading(true)
+        let list=[];
+        const querySnapshot =await getService("adminUsers")
+        querySnapshot.forEach((doc) => {
+        list.push({id:doc.id,
+                  firstValue:doc.data().email,
+                  secondValue:(new Date(doc.data().time.toDate())).toDateString(),
+                  ...doc.data()})
+                                    });
+
+      setRecords(list)
+      setLoading(false)
+     console.log(list)
+      
+      };
+
+    useEffect(()=>{
+        if(!openPopup){
+        handelFetch()
+        }
+       },[openPopup])
+
+      const updateStatus = async (id,status) => {
+          const newStatus= status==="blocked"?"active":"blocked";
+          await updateService('adminUsers',id,{status:newStatus})
+          const updatedData = records.map((item) => {
+              
+            if(item.id === id){
+                item.status=newStatus
+            } 
+            return item
+            
+            });   
+          setRecords(updatedData) 
+      }
+    return (
+        <PageWrapper>
+            
+     { loading ? (
+        <Stack spacing={1}>
+          <Skeleton variant="text" height={100} />
+          <Skeleton variant="text" height={20} />
+          <Skeleton variant="text" height={20} />
+          <Skeleton variant="rectangular" height={300} />
+        </Stack>):
+            records ?
+
+        <>
+            <Table records={records} 
+    
+            setOpenPopup={setOpenPopup} 
+            headCells={headCells}
+    //   openPopup={openPopup}
+           viewDetailsButton={false}
+           editButton={false}
+           updateStatus={updateStatus}
+/>
+            
+            <Popup
+                title="Add Admin User"
+                openPopup={openPopup}
+                setOpenPopup={setOpenPopup}
+            >
+                    <AddAdminUser
+                     setOpenPopup={setOpenPopup}
+                    />
+            </Popup>
+            </>
+            :<></>
+}
+</PageWrapper>
+
+    )
+}
+
+  
